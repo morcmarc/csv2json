e9fdaaacc0dbf2f5a551c96586c5c1893c30b4bc
Marcell Jusztin <hello@morcmarc.com>
2014-11-13 10:11:05 +0000

    Refactored converter

--- convert.go
+++ convert.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"unicode/utf8"
 
 	. "github.com/morcmarc/csv2json/converter"
 
@@ -21,14 +23,25 @@ var convertCommand = cli.Command{
 		cli.StringFlag{
 			Name:  "delimiter, d",
 			Value: ",",
+			Usage: "field delimiter, set to ',' by default",
 		},
 		cli.StringFlag{
-			Name:  "quote, q",
-			Value: "\"",
+			Name:  "comment, c",
+			Value: "",
+			Usage: "comment character for start of line",
 		},
-		cli.StringFlag{
-			Name:  "escape, e",
-			Value: "\\",
+		cli.IntFlag{
+			Name:  "fields, f",
+			Value: 0,
+			Usage: "number of expected fields per record",
+		},
+		cli.BoolFlag{
+			Name:  "lazy, l",
+			Usage: "allow lazy quotes",
+		},
+		cli.BoolFlag{
+			Name:  "trim, t",
+			Usage: "trim leading space",
 		},
 	},
 }
@@ -37,16 +50,41 @@ func convertAction(c *cli.Context) {
 	if len(c.Args()) == 0 {
 		log.Fatalln("No input given")
 	}
+	iFile := c.Args()[0]
+	csv, err := os.Open(iFile)
+	if err != nil {
+		log.Fatalf("Could not open file: %s", iFile)
+	}
 
 	oFile := c.String("output")
 	if oFile == "" {
 		log.Fatalln("No output file")
 	}
 
-	converter := &Converter{
-		Delimiter: c.String("delimiter"),
-		Quote:     c.String("quote"),
-		Escape:    c.String("escape"),
+	converter := NewConverter(csv)
+
+	if d := c.String("delimiter"); d != "," {
+		db := []byte(d)
+		dr, _ := utf8.DecodeRune(db)
+		converter.SetDelimiter(dr)
+	}
+
+	if c := c.String("comment"); c != "" {
+		cb := []byte(c)
+		cr, _ := utf8.DecodeRune(cb)
+		converter.SetComment(cr)
+	}
+
+	if f := c.Int("fields"); f != 0 {
+		converter.SetFieldsPerRecord(f)
+	}
+
+	if l := c.Bool("lazy"); l {
+		converter.SetLazyQuotes(l)
+	}
+
+	if t := c.Bool("trim"); t {
+		converter.SetTrim(t)
 	}
 
 	converter.Run()
--- converter/converter.go
+++ converter/converter.go
@@ -1,9 +1,39 @@
 package converter
 
+import (
+	"encoding/csv"
+	"io"
+)
+
 type Converter struct {
-	Delimiter string
-	Quote     string
-	Escape    string
+	Input  io.Reader
+	reader *csv.Reader
+}
+
+func NewConverter(csvInput io.Reader) *Converter {
+	converter := &Converter{}
+	converter.reader = csv.NewReader(csvInput)
+	return converter
+}
+
+func (c *Converter) SetDelimiter(d rune) {
+	c.reader.Comma = d
+}
+
+func (c *Converter) SetComment(d rune) {
+	c.reader.Comment = d
+}
+
+func (c *Converter) SetFieldsPerRecord(f int) {
+	c.reader.FieldsPerRecord = f
+}
+
+func (c *Converter) SetLazyQuotes(l bool) {
+	c.reader.LazyQuotes = l
+}
+
+func (c *Converter) SetTrim(t bool) {
+	c.reader.TrimLeadingSpace = t
 }
 
 func (c *Converter) Run() {
